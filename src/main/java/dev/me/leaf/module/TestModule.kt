4f2d37fff29442a528817d7cd9bb975eaeb5901e
NoneMinecraft <2771776476@qq.com>
2025-10-04 00:28:54 +0800

    delete: 删除多余文件

--- src/main/java/dev/me/leaf/module/TestModule.kt
+++
@@ -1,8 +0,0 @@
-package dev.me.leaf.module
-
-import net.nonemc.leaf.module.Module
-import net.nonemc.leaf.module.ModuleCategory
-import net.nonemc.leaf.module.ModuleInfo
-
-@ModuleInfo(name = "TestModule", category = ModuleCategory.COMBAT)
-class TestModule : Module() {}
