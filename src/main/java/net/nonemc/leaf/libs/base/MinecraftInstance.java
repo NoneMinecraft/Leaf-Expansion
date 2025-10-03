08d0fbe97928ed2afe341b34dd215f873c73c7c8
NoneMinecraft <2771776476@qq.com>
2025-10-04 00:29:00 +0800

    delete: 删除多余文件

--- src/main/java/net/nonemc/leaf/libs/base/MinecraftInstance.java
+++
@@ -1,7 +0,0 @@
-package net.nonemc.leaf.libs.base;
-
-import net.minecraft.client.Minecraft;
-
-public class MinecraftInstance {
-    protected static final Minecraft mc = Minecraft.getMinecraft();
-}
