25605f9d75fc8190f782c93567a6b72952513153
NoneMinecraft <2771776476@qq.com>
2025-10-04 00:29:07 +0800

    delete: 删除多余文件

--- src/main/java/net/nonemc/leaf/value/FontValue.kt
+++
@@ -1,5 +0,0 @@
-﻿package net.nonemc.leaf.value
-
-import net.minecraft.client.gui.FontRenderer
-
-abstract class FontValue(valueName: String, value: FontRenderer) : Value<FontRenderer>(valueName, value)
